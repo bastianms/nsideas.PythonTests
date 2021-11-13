256e62a1880d8fa4c285621ea8ded290766e469e
Franco Morales Castro <franco.morales@outlook.com>
2021-11-13 00:35:41 +0000

    Encriptar texto

--- Ejemplo5/Test.py
+++ Ejemplo5/Test.py
@@ -1,5 +1,11 @@
-from cryp
+from Crypto.Hash import MD5
+from Crypto.Hash import SHA256
 
-hash = SHA256.new()
-hash.update('message')
-hash.digest()
+#hash = MD5.new()
+#hash2 = SHA256.new()
+hash = MD5.new(b'123456')
+hash2 = SHA256.new(b'123456')
+hash.update(b'hola')
+hash2.update(b'hola')
+print(hash.hexdigest())
+print(hash2.hexdigest())
