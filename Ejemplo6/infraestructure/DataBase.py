daad389299fe61410ec6a932fa5f573e7fec18d1
francolmc <franco.morales@outlook.com>
2021-11-19 21:50:23 -0300

    Clase viernes 1P/11

---
+++ Ejemplo6/infraestructure/DataBase.py
@@ -0,0 +1,47 @@
+import mysql.connector
+
+class MySQLDataBase:
+    def __init__(self, host = '', username = '', password = '', database_name = '') -> None:
+        self.__host = host
+        self.__username = username
+        self.__password = password
+        self.__database_name = database_name
+        self.__connection = None
+        self.__cursor = None
+
+    def Open(self):
+        self.__connection = mysql.connector.connect(
+            host=self.__host,
+            user=self.__username,
+            database=self.__database_name,
+            password=self.__password
+        )
+        self.__cursor = self.__connection.cursor()
+        print('Conexion exitosa.')
+
+    def Close(self):
+        self.__connection.close()
+        print('Conexion con base de datos cerrada.')
+
+    def fetchAll(self, sql):
+        try:
+            self.__cursor.execute(sql)
+            datos = self.__cursor.fetchall()
+            return datos
+        except Exception as e:
+            raise
+
+    def fetchOne(self, sql):
+        try:
+            self.__cursor.execute(sql)
+            datos = self.__cursor.fetchone()
+            return datos
+        except Exception as e:
+            raise
+    
+    def excuteQuery(self, sql):
+        try:
+            self.__cursor.execute(sql)
+            self.__connection.commit()
+        except:        
+            raise
